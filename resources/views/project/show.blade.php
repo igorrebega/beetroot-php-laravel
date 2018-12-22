1eb2683c64844dd0ebbe28c925d7c9e1c7de9b85
Igor Rebega <igorrebega@gmail.com>
2018-12-22 18:20:58 +0200

    Task relation

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -6,4 +6,13 @@
     <p>
         {{$project->description}}
     </p>
+
+    <div>
+        @foreach($project->tasks as $task)
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="taskCheckbox">
+                <label class="form-check-label" for="taskCheckbox">{{$task->description}}</label>
+            </div>
+        @endforeach
+    </div>
 @endsection
